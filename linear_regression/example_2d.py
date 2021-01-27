5a68a1bc0333eafa06a35e2b53c02f199f21ee50
Mahtat <yassinmhtat@gmail.com>
2021-01-27 21:23:55 +0100

    Gradient descent

--- linear_regression/example_2d.py
+++ linear_regression/example_2d.py
@@ -9,11 +9,43 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_regression
 
 
-def model(data_x, param):
+def model(data_x, parameters):
     """ This function returns the linear model
 
     """
-    return data_x @ param
+    return data_x @ parameters
+
+
+def cost_function(data_x, data_y, parameters):
+    """ Returns the cost function for our model.
+
+    """
+    # Residual sum of squares
+    rss = np.sum((data_x @ parameters - data_y) ** 2)
+
+    # Number of rows times 2
+    nb_rows_x2 = 2 * data_x.shape[0]
+
+    return rss / nb_rows_x2
+
+
+def gradient(data_x, data_y, parameters):
+    """ Returns gradient of a function.
+
+    """
+    return data_x.T @ (data_x @ parameters - data_y) / data_x.shape[0]
+
+
+def gradient_descent(data_x, data_y, parameters, learn_rate, nb_iterations):
+    """ Finding a local minimum of a differentiable function by gradient
+    descent algorithm.
+
+    """
+
+    for _i in range(nb_iterations):
+        parameters -= learn_rate * gradient(data_x, data_y, parameters)
+
+    return parameters
 
 
 def main():
@@ -36,11 +68,11 @@ def main():
     #################
     ##    Model    ##
     #################
-    # parameter
-    param = np.random.randn(2, 1)
+    # initial parameters
+    init_params = np.random.randn(2, 1)
 
     # initial model
-    init_model = model(data_x, param)
+    init_model = model(data_x, init_params)
 
     # show initial model
     plt.plot(data_x, init_model, c='r')
@@ -48,6 +80,12 @@ def main():
     #########################
     ##    cost function    ##
     #########################
+    # show cost function for initial params
+    print(cost_function(data_x, data_y, init_params))
+
+    ####################
+    ##    training    ##
+    ####################
 
     plt.show()
 
